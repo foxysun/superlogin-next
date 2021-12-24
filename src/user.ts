bfef84cef68677be8a9ecb811ccc269cb44f8c6e
ktran <kayo@foxysun.com>
2021-12-24 10:37:57 +0700

    Fixed invalid userID format

--- src/user.ts
+++ src/user.ts
@@ -336,15 +336,18 @@ export class User {
       let users = [];
       for (let i = 1; i < rows.length; i++) {
         const user = await this.userDB.get(rows[i].id);
-        const profile = user.profile;
 
-        users.push({
-          id: rows[i].id,
-          firstName: profile && profile.firstName,
-          lastName: profile && profile.lastName,
-          accountType: profile && profile.accountType,
-          email: profile && profile.email
-        });
+        if (rows[i].id.length > 15) {
+          const profile = user.profile;
+
+          users.push({
+            id: rows[i].id,
+            firstName: profile && profile.firstName,
+            lastName: profile && profile.lastName,
+            accountType: profile && profile.accountType,
+            email: profile && profile.email
+          });
+        }
       }
       return users;
     } else {
